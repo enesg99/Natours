11d78b4078574a743fd60d280317bef6c8d18a9b
Enes <enesgurleyen@hotmail.de>
2025-01-03 03:09:46 +0100

    Final changes for deployment

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 
 mongoose.connect(DB).then(con => { console.log('DB connection successful')})
 
-const port = process.env.PORT ||  3000;
+const PORT = process.env.PORT ||  3000;
 const server = app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${PORT}`);
 });
 
 process.on('unhandledRejection', (err) => {
